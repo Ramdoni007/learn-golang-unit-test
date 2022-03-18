9c3079235fb89e50c81ae2ed4c47d535f9797a46
Ramdoni007 <ramd42156@gmail.com>
2022-03-18 11:27:15 +0700

    Added Materi Mock In My Learn Project

---
+++ service/category_service_test.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"learn-golang-unit-test/entity"
+	"learn-golang-unit-test/repository"
+	"testing"
+)
+
+var categoryRepository = &repository.CategoryRepositoryMock{Mock:mock.Mock{}}
+var categoryService = CategoryService{Repository:categoryRepository}
+
+func TestCategoryService_GetNotFound(t *testing.T)  {
+	// ProgramMock
+	categoryRepository.Mock.On("FindById","1").Return(nil)
+
+	category,err := categoryService.Get("1")
+	assert.Nil(t,category)
+	assert.NotNil(t,err)
+}
+
+
+func TestCategoryService_GetSucces(t *testing.T)  {
+
+	category := entity.Category{
+		Id: "1",
+		Name: "Laptop",
+	}
+
+	// ProgramMock
+	categoryRepository.Mock.On("FindById","2").Return(category)
+
+	result,err := categoryService.Get("2")
+	assert.Nil(t,err)
+	assert.NotNil(t,result)
+	assert.Equal(t,category.Name,result.Name)
+	assert.Equal(t,category.Id,result.Id)
+}
