0f3ba199afbb76e958d363da0b707db95039b8ac
Ramdoni007 <ramd42156@gmail.com>
2022-03-18 09:34:21 +0700

    Added Materi TableTest in My Project Learn

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -3,7 +3,37 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
+
+func TestTableHelloWorld(t *testing.T) {
+	test := []struct {
+		name     string
+		request  string
+		expected string
+	}{
+		{
+			name:     "Doni",
+			request:  "Doni",
+			expected: "Hello Doni",
+		},
+
+		{
+			name:     "Ahmad",
+			request:  "Ahmad",
+			expected: "Hello Ahmad",
+		},
+	}
+
+	for _, test := range test {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWorld(test.request)
+			require.Equal(t, test.expected, result)
+		})
+	}
+
+}
 
 func TestHeloWorldDoni(t *testing.T) {
 	result := HelloWorld("Doni")
